c2c119a360393b5c7c29ab8e3754397ad3398955
Naiden Nikolaev Petrov <naiden.petrov.31.12.00@gmail.com>
2022-10-20 19:38:44 +0300

    Create SQLQuery2.sql

---
+++ MS_SQL/DatabasesIntroductionExercise/SQLQuery2.sql
@@ -0,0 +1,25 @@
+CREATE TABLE Users (
+	Id BIGINT PRIMARY KEY IDENTITY,
+	UserName NVARCHAR(30) UNIQUE NOT NULL,
+	[Password] VARCHAR(26) NOT NULL,
+	ProfilePicture VARBINARY(MAX),
+	CHECK(DATALENGTH(ProfilePicture) <= 921600),
+	LastLoginTime DATETIME2,
+	IsDeleted BIT NOT NULL
+)
+
+DROP TABLE Users
+
+INSERT INTO Users(Username, [Password],ProfilePicture, LastLoginTime, IsDeleted) VALUES
+	('Pesho', '123', NULL, NULL, 0),
+	('Gosho', '123',NULL,NULL,0),
+	('Ivan', '123',NULL,NULL,0),
+	('Test', '123',NULL,NULL,1),
+	('Test123', '123',NULL,NULL,1)
+
+ALTER TABLE Users
+DROP CONSTRAINT PK__Users__3214EC0775A46987
+
+ALTER TABLE Users
+ADD CONSTRAINT PK_CompositeIdUsername
+PRIMARY KEY(Id,Username)
