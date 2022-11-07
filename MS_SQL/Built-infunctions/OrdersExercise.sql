a3778300f066f9b7dbd34375c2b20f612cdd03dc
Naiden Nikolaev Petrov <naiden.petrov.31.12.00@gmail.com>
2022-11-07 22:58:35 +0200

    Update OrdersExercise.sql

--- MS_SQL/Built-infunctions/OrdersExercise.sql
+++ MS_SQL/Built-infunctions/OrdersExercise.sql
@@ -10,3 +10,22 @@ FROM Customers
 GO
 
 SELECT * FROM v_PublicPaymentsInfo
+
+-- CHARINDEX - locates s specific pattern
+SELECT CHARINDEX('is', 'This is long text')
+
+-- STUFF - insert substring in specific position
+SELECT STUFF('This is a bad idea', 11, 3, 'good')	
+
+-- FORMAT - returns value formatted with the specified format
+SELECT FORMAT(67.23, 'C', 'bg-BG')
+
+-- Problem Pallets
+SELECT 
+	[NAME], 
+	 CEILING(CEILING(CAST(Quantity AS float)/ BoxCapacity) / PalletCapacity) AS Pallets
+FROM Products
+
+-- DATEPART - exctract a segment from a date as an int
+SELECT 
+	DATEPART(WEEK, '2022-12-31')
