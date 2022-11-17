1d14f15334d292828a42ec8320bbc480081b5202
Naiden Nikolaev Petrov <naiden.petrov.31.12.00@gmail.com>
2022-11-17 23:18:51 +0200

    Update Exercises.sql

--- MS_SQL/DataAggregationExercise/Exercises.sql
+++ MS_SQL/DataAggregationExercise/Exercises.sql
@@ -33,3 +33,35 @@ SELECT
 FROM WizzardDeposits AS w
 GROUP BY w.DepositGroup, w.MagicWandCreator
 ORDER BY w.MagicWandCreator, w.DepositGroup ASC
+
+-- Problem 9. Age Groups
+SELECT 
+CASE
+	WHEN Age BETWEEN 0 AND 10 THEN '[0-10]'
+	WHEN Age BETWEEN 11 AND 20 THEN '[11-20]'
+	WHEN Age BETWEEN 21 AND 30 THEN '[21-30]'
+	WHEN Age BETWEEN 31 AND 40 THEN '[31-40]'
+	WHEN Age BETWEEN 41 AND 50 THEN '[41-50]'
+	WHEN Age BETWEEN 51 AND 60 THEN '[51-60]'
+	ELSE '[61+]'
+END AS [AgeGroup],
+COUNT(*) AS [WizardCount]
+FROM WizzardDeposits
+GROUP BY  
+CASE
+	WHEN Age BETWEEN 0 AND 10 THEN '[0-10]'
+	WHEN Age BETWEEN 11 AND 20 THEN '[11-20]'
+	WHEN Age BETWEEN 21 AND 30 THEN '[21-30]'
+	WHEN Age BETWEEN 31 AND 40 THEN '[31-40]'
+	WHEN Age BETWEEN 41 AND 50 THEN '[41-50]'
+	WHEN Age BETWEEN 51 AND 60 THEN '[51-60]'
+	ELSE '[61+]'
+END
+
+-- Problem 12. * Rich Wizard, Poor Wizard
+SELECT SUM([Difference]) AS SumDifference FROM (SELECT FirstName AS [Host Wizard],
+	   DepositAmount AS [Host Wizard Deposit],
+	   LEAD(FirstName) OVER(ORDER BY Id) AS [Guest Wizard],
+	   LEAD(DepositAmount) OVER(ORDER BY Id) AS [Guest Wizard Deposit],
+	   DepositAmount -  LEAD(DepositAmount) OVER(ORDER BY Id) AS [Difference]
+FROM WizzardDeposits) AS DiffTable
