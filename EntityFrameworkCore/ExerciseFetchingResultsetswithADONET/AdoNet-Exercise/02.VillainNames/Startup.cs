1aee7f6022c317d1a88561b217de810bfbd627a4
Naiden Nikolaev Petrov <naiden.petrov.31.12.00@gmail.com>
2023-01-04 20:34:28 +0200

    Exercise

---
+++ EntityFrameworkCore/ExerciseFetchingResultsetswithADONET/AdoNet-Exercise/02.VillainNames/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System.Data.SqlClient;
+
+namespace _02.VillainNames
+{
+    class Startup
+    {
+        private static string conectionString =
+            "Server=DESKTOP-ERAGAG4\\SQLEXPRESS;" +
+            "Database=MinionsDB;" +
+            "Integrated Security=true";
+        private static SqlConnection connection = new SqlConnection(conectionString);
+
+        static void Main(string[] args)
+        {
+            connection.Open();
+
+            using (connection)
+            {
+                var queryText = @"SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount  
+                                      FROM Villains AS v 
+                                      JOIN MinionsVillains AS mv ON v.Id = mv.VillainId 
+                                  GROUP BY v.Id, v.Name 
+                                    HAVING COUNT(mv.VillainId) > 3 
+                                  ORDER BY COUNT(mv.VillainId)";
+                var command = new SqlCommand(queryText, connection);
+
+                try
+                {
+                    var reader = command.ExecuteReader();
+                     
+                    using (reader)
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"{reader["Name"]} - {reader["MinionsCount"]}");
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("There was problem processing you request!");
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
---
+++ EntityFrameworkCore/ExerciseFetchingResultsetswithADONET/AdoNet-Exercise/06.RemoveVillain/Startup.cs
@@ -0,0 +1,67 @@
+﻿namespace _06.RemoveVillain
+{
+    using System.Data.SqlClient;
+
+    class Startup
+    {
+        private static string connectionString =
+            "Server=DESKTOP-ERAGAG4\\SQLEXPRESS;" +
+            "Database=MinionsDB;" +
+            "Integrated Security=true";
+        private static SqlConnection connection = new SqlConnection(connectionString);
+        private static SqlTransaction transaction;
+
+        static void Main(string[] args)
+        {
+            var id = int.Parse(Console.ReadLine());
+
+            connection.Open();
+
+            using (connection)
+            {
+                transaction = connection.BeginTransaction();
+                try
+                {
+                    var command = new SqlCommand();
+                    command.Connection = connection;
+                    command.Transaction = transaction;
+                    command.CommandText = "SELECT Name FROM Villains WHERE Id = @villainId";
+                    command.Parameters.AddWithValue("@villainId", id);
+
+                    var value = command.ExecuteScalar();
+
+                    if (value == null)
+                    {
+                        throw new ArgumentException("No such villain was found.");
+                    }
+
+                    var villianName = (string)value;
+
+                    command.CommandText = @"DELETE FROM MinionsVillains 
+                                            WHERE VillainId = @villainId";
+                    var minionsDeleted = command.ExecuteNonQuery();
+
+                    command.CommandText = @"DELETE FROM Villains
+                                            WHERE Id = @villainId";
+                    command.ExecuteNonQuery();
+
+                    transaction.Commit();
+                    Console.WriteLine($"{villianName} was deleted.");
+                    Console.WriteLine($"{minionsDeleted} minions were released.");
+                }
+                catch (ArgumentException ane)
+                {
+                    try
+                    {
+                        Console.WriteLine(ane.Message);
+                        transaction.Rollback();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+            }
+        }
+    }
+}
