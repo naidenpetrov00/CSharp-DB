0825dc96c0b0969fd1ab306ee8bd8e15ed0d63b3
Naiden Nikolaev Petrov <naiden.petrov.31.12.00@gmail.com>
2023-01-05 20:49:16 +0200

    Exercise

---
+++ EntityFrameworkCore/ExerciseFetchingResultsetswithADONET/AdoNet-Exercise/03.MinionNames/Startup.cs
@@ -0,0 +1,48 @@
+﻿namespace _03.MinionNames
+{
+    using System.Data.SqlClient;
+
+    class Startup
+    {
+        private static string connectionString =
+            "Server=DESKTOP-ERAGAG4\\SQLEXPRESS;" +
+            "Database=MinionsDB;" +
+            "Integrated Security=true";
+        private static SqlConnection connection = new SqlConnection(connectionString);
+
+        static void Main(string[] args)
+        {
+            var id = int.Parse(Console.ReadLine());
+
+            connection.Open();
+
+            using (connection)
+            {
+                var querryString = @"SELECT ROW_NUMBER() OVER (ORDER BY m.Name) as RowNum,
+                                         m.Name, 
+                                         m.Age
+                                     FROM MinionsVillains AS mv
+                                     JOIN Minions As m ON mv.MinionId = m.Id
+                                     WHERE mv.VillainId = @Id
+                                     ORDER BY m.Name";
+
+                try
+                {
+                    var command = new SqlCommand(querryString, connection);
+                    command.Parameters.AddWithValue("@Id", id);
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["RowNum"]}. {reader["Name"]} {reader["Age"]}");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("There was problem with executing your request!");
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
---
+++ EntityFrameworkCore/ExerciseFetchingResultsetswithADONET/AdoNet-Exercise/04.AddMinion/Startup.cs
@@ -0,0 +1,69 @@
+﻿namespace _04.AddMinion
+{
+    using System;
+    using System.Linq;
+    using System.Data.SqlClient;
+
+    class Startup
+    {
+        private static string connectionString =
+            "Server=DESKTOP-ERAGAG4\\SQLEXPRESS;" +
+            "Database=MinionsDB;" +
+            "Integrated Security=true";
+        private static SqlConnection connection = new SqlConnection(connectionString);
+
+        static void Main(string[] args)
+        {
+            var minionInformation = Console.ReadLine()
+                                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                    .Where(e => e != "Minion:")
+                                    .ToArray();
+            var minionName = minionInformation[0];
+            var minionAge = minionInformation[1];
+            var minionTown = minionInformation[2];
+            var villianInformation = Console.ReadLine().Replace("Villain: ", string.Empty);
+
+            connection.Open();
+
+            using (connection)
+            {
+                var transaction = connection.BeginTransaction();
+
+                var querryText = @"INSERT INTO MinionsVillains (MinionId, VillainId) VALUES (@villainId, @minionId)
+                                   INSERT INTO Villains (Name, EvilnessFactorId)  VALUES (@villainName, 4)
+                                   INSERT INTO Minions (Name, Age, TownId) VALUES (@nam, @age, @townId)
+                                   INSERT INTO Towns (Name) VALUES (@townName)
+                                   SELECT Id FROM Towns WHERE Name = @townName";
+
+                try
+                {
+                    var command = new SqlCommand(querryText, connection);
+                    command.Transaction = transaction;
+                    command.Parameters.AddWithValue("@villainId", villianInformation);
+                    command.Parameters.AddWithValue("@nam", minionName);
+                    command.Parameters.AddWithValue("@age", minionAge);
+                    command.Parameters.AddWithValue("@townName", minionTown);
+
+                    //Commit the transaction.
+                    transaction.Commit();
+                    Console.WriteLine("Request executed successfully");
+                }
+                catch (Exception ex)
+                {
+                    // Handle the exception if the transaction fails to commit.
+                    Console.WriteLine(ex.Message);
+
+                    try
+                    {
+                        // Attempt to roll back the transaction.
+                        transaction.Rollback();
+                    }
+                    catch (Exception exRollback)
+                    {
+                        Console.WriteLine(exRollback.Message);
+                    }
+                }
+            }
+        }
+    }
+}
