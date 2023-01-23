bfec8e657d8cb36934e97b41747a0af417015479
Naiden Nikolaev Petrov <naiden.petrov.31.12.00@gmail.com>
2023-01-23 22:15:53 +0200

    EF Exercise

--- EntityFrameworkCore/EntityFrameworkIntroductionExercise/EntityFramwork-Exercise/SoftUni/Startup.cs
+++ EntityFrameworkCore/EntityFrameworkIntroductionExercise/EntityFramwork-Exercise/SoftUni/Startup.cs
@@ -14,7 +14,11 @@
 
             //var result = GetEmployeesFullInformation(db);
             //var result = GetEmployeesWithSalaryOver50000(db);
-            var result = GetEmployeesFromResearchAndDevelopment(db);
+            //var result = GetEmployeesFromResearchAndDevelopment(db);
+            //var result = AddNewAddressToEmployee(db);
+            //var result = IncreaseSalaries(db);
+            var result = GetEmployeesByFirstNameStartingWithSa(db);
+
 
             Console.WriteLine(result);
         }
@@ -88,6 +92,101 @@
             }
 
             return sb.ToString().TrimEnd();
+        }
+
+        public static string AddNewAddressToEmployee(SoftUniContext db)
+        {
+            var sb = new StringBuilder();
+
+            var address = new Address()
+            {
+                AddressText = "Vitoshka 15",
+                TownId = 4
+            };
+
+            var employee = db.Employees
+                .First(e => e.LastName == "Nakov");
+
+            employee.Address = address;
+
+            db.SaveChanges();
+
+            var addressesText = db.Employees
+                .OrderByDescending(e => e.AddressId)
+                .Select(e => new
+                {
+                    e.Address.AddressText
+                })
+                .Take(10)
+                .ToList();
+
+            foreach (var addressText in addressesText)
+            {
+                sb.AppendLine(addressText.AddressText);
+            }
+
+            return sb.ToString().Trim();
+        }
+
+        public static string IncreaseSalaries(SoftUniContext db)
+        {
+            var sb = new StringBuilder();
+
+            var employees = db.Employees
+                .Where(e => e.Department.Name == "Engineering"
+                || e.Department.Name == "Tool Design"
+                || e.Department.Name == "Marketing"
+                || e.Department.Name == "Information Services");
+
+            foreach (var employee in employees)
+            {
+                employee.Salary *= 1.12m;
+            }
+
+            db.SaveChanges();
+
+            var employeesToPrint = employees
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.Salary
+                })
+                .OrderBy(e => e.FirstName)
+                .ThenBy(e => e.LastName)
+                .ToList();
+
+            foreach (var e in employeesToPrint)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} ({e.Salary:F2})");
+            }
+
+            return sb.ToString().Trim();
+        }
+
+        public static string GetEmployeesByFirstNameStartingWithSa(SoftUniContext db)
+        {
+            var sb = new StringBuilder();
+
+            var employees = db.Employees
+                .Where(e => e.FirstName.StartsWith("Sa"))
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.JobTitle,
+                    e.Salary
+                })
+                .OrderBy(e => e.FirstName)
+                .ThenBy(e => e.LastName)
+                .ToList();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} - {employee.JobTitle} - (${employee.Salary:F2})");
+            }
+
+            return sb.ToString().Trim();
         }
     }
 }
