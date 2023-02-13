842cfd9ba1938c851b8a959ac3db2a493112dd85
Naiden Nikolaev Petrov <naiden.petrov.31.12.00@gmail.com>
2023-02-13 15:43:40 +0200

    Auto Mapper Exercise

--- EntityFrameworkCore/AutoMappingObjectsExercise/FastFood.Web/Startup.cs
+++ EntityFrameworkCore/AutoMappingObjectsExercise/FastFood.Web/Startup.cs
@@ -40,6 +40,8 @@ namespace FastFood.Web
                     Configuration.GetConnectionString("DefaultConnection")));
 
             //TODO add Automapper configuration
+            services.AddAutoMapper(cfg =>
+                cfg.AddProfile<FastFoodProfile>());
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
