fe00d5ce3e2e6fd0efbb22efd9d29dd5f8eafe88
Naiden Nikolaev Petrov <naiden.petrov.31.12.00@gmail.com>
2023-02-15 20:50:45 +0200

    JSON Processing Lab

---
+++ EntityFrameworkCore/JSONProcessing/JSONDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace JSONDemo
+{
+	using Newtonsoft.Json;
+
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var inputJson = File.ReadAllText("../../../products.json");
+
+			var products = JsonConvert.DeserializeObject<ProductDto[]>(inputJson);
+
+			var json = JsonConvert.SerializeObject(products, Formatting.Indented);
+
+			Console.WriteLine(json);
+		}
+	}
+}
---
+++ EntityFrameworkCore/JSONProcessing/ViewModels/ProductDto.cs
@@ -0,0 +1,13 @@
+﻿namespace JSONDemo
+{
+	public class ProductDto
+	{
+		public string Name { get; set; }
+
+		public float Price { get; set; }
+
+		public int SellerId { get; set; }
+
+		public int? BuyerId { get; set; }
+	}
+}
