a4f2c214a32b4179561756d8cbabec90d9e4b0c4
Naiden Nikolaev Petrov <naiden.petrov.31.12.00@gmail.com>
2023-02-17 19:52:18 +0200

    JSON Processing Exercise

--- EntityFrameworkCore/JSONProccessingExercise/ProductShop/StartUp.cs
+++ EntityFrameworkCore/JSONProccessingExercise/ProductShop/StartUp.cs
@@ -1,6 +1,10 @@
 ﻿namespace ProductShop
 {
+	using System;
+	using System.IO;
+	using Newtonsoft.Json;
 	using ProductShop.Data;
+	using ProductShop.Models;
 
 	public class StartUp
 	{
@@ -8,7 +12,52 @@
 		{
 			using var db = new ProductShopContext();
 
-			db.Database.EnsureCreated();
+			var usersJson = File.ReadAllText("./../../../Datasets/users.json");
+			var productsJson = File.ReadAllText("./../../../Datasets/products.json");
+			var categoriesJson = File.ReadAllText("./../../../Datasets/categories.json");
+			var categoryProductsJson = File.ReadAllText("./../../../Datasets/categories-products.json");
+
+			Console.WriteLine(ImportCategoryProducts(db, categoryProductsJson));
+		}
+		 
+		public static string ImportUsers(ProductShopContext db, string inputJson)
+		{
+			var users = JsonConvert.DeserializeObject<User[]>(inputJson);
+
+			db.Users.AddRange(users);
+			db.SaveChanges();
+
+			return $"Successfully imported {users.Length}";
+		}
+
+		public static string ImportProducts(ProductShopContext db, string inputJson)
+		{
+			var products = JsonConvert.DeserializeObject<Product[]>(inputJson);
+
+			db.Products.AddRange(products);
+			db.SaveChanges();
+
+			return $"Successfully imported {products.Length}";
+		}
+
+		public static string ImportCategories(ProductShopContext db, string inputJson)
+		{
+			var categories = JsonConvert.DeserializeObject<Category[]>(inputJson);
+
+			db.Categories.AddRange(categories);
+			db.SaveChanges();
+
+			return $"Successfully imported {categories.Length}";
+		}
+
+		public static string ImportCategoryProducts(ProductShopContext db, string inputJson)
+		{
+			var categoryProducts = JsonConvert.DeserializeObject<CategoryProduct[]>(inputJson);
+
+			db.CategoryProducts.AddRange(categoryProducts);
+			db.SaveChanges();
+
+			return $"Successfully imported {categoryProducts.Length}";
 		}
 	}
 }
